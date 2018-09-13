eb936f843d3572a310cb339d22052b681363e9bd
saiintellect <venkata.sanagapalli@intellectdesign.com>
2018-09-13 10:05:53 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { ComboComponent } from './combo/combo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RadiofieldComponent } from './radiofield/radiofield.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    ComboComponent,
+    HeaderComponent,
+    FooterComponent,
+    RadiofieldComponent
   ],
   imports: [
     BrowserModule
